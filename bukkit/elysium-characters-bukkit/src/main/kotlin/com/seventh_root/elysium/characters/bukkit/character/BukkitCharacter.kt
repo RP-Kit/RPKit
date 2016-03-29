2261f4e730f87642857a93001dd3b6bf134f4102
Ross Binden <alyphen@gmail.com>
2016-03-29 23:09:04 +0100

    Merge branch 'bugfix/saveOffhand' into develop

--- bukkit/elysium-characters-bukkit/src/main/kotlin/com/seventh_root/elysium/characters/bukkit/character/BukkitCharacter.kt
+++ bukkit/elysium-characters-bukkit/src/main/kotlin/com/seventh_root/elysium/characters/bukkit/character/BukkitCharacter.kt
@@ -14,7 +14,7 @@ import org.bukkit.Location
 import org.bukkit.inventory.ItemStack
 import java.util.*
 
-class BukkitCharacter private constructor(id: Int, player: ElysiumPlayer, name: String, gender: Gender, age: Int, race: Race, description: String, dead: Boolean, location: Location, inventoryContents: Array<ItemStack>, helmet: ItemStack?, chestplate: ItemStack?, leggings: ItemStack?, boots: ItemStack?, health: Double, maxHealth: Double, mana: Int, maxMana: Int, foodLevel: Int, thirstLevel: Int) : ElysiumCharacter {
+class BukkitCharacter private constructor(id: Int, player: ElysiumPlayer, name: String, gender: Gender, age: Int, race: Race, description: String, dead: Boolean, location: Location, inventoryContents: Array<ItemStack>, offhand: ItemStack?, helmet: ItemStack?, chestplate: ItemStack?, leggings: ItemStack?, boots: ItemStack?, health: Double, maxHealth: Double, mana: Int, maxMana: Int, foodLevel: Int, thirstLevel: Int) : ElysiumCharacter {
 
     class Builder
     @Suppress("UNCHECKED_CAST")
@@ -30,6 +30,7 @@ class BukkitCharacter private constructor(id: Int, player: ElysiumPlayer, name: 
         private var dead: Boolean
         private var location: Location
         private var inventoryContents: MutableList<ItemStack>
+        private var offhand: ItemStack? = null
         private var helmet: ItemStack? = null
         private var chestplate: ItemStack? = null
         private var leggings: ItemStack? = null
@@ -62,6 +63,7 @@ class BukkitCharacter private constructor(id: Int, player: ElysiumPlayer, name: 
             dead = plugin.config.getBoolean("characters.defaults.dead")
             location = Bukkit.getWorlds()[0].spawnLocation
             inventoryContents = plugin.config.getList("characters.defaults.inventory-contents") as ArrayList<ItemStack>
+            offhand = plugin.config.getItemStack("characters.defaults.offhand")
             helmet = plugin.config.getItemStack("characters.defaults.helmet")
             chestplate = plugin.config.getItemStack("characters.defaults.chestplate")
             leggings = plugin.config.getItemStack("characters.defaults.leggings")
@@ -135,6 +137,11 @@ class BukkitCharacter private constructor(id: Int, player: ElysiumPlayer, name: 
             return this
         }
 
+        fun offhand(offhand: ItemStack?): Builder {
+            this.offhand = offhand
+            return this
+        }
+
         fun helmet(helmet: ItemStack?): Builder {
             this.helmet = helmet
             return this
@@ -197,6 +204,7 @@ class BukkitCharacter private constructor(id: Int, player: ElysiumPlayer, name: 
                     dead,
                     location,
                     inventoryContents.toTypedArray(),
+                    offhand,
                     helmet,
                     chestplate,
                     leggings,
@@ -233,6 +241,7 @@ class BukkitCharacter private constructor(id: Int, player: ElysiumPlayer, name: 
     override var isDead: Boolean = dead
     var location: Location = location
     var inventoryContents: Array<ItemStack> = inventoryContents
+    var offhand: ItemStack? = offhand
     var helmet: ItemStack? = helmet
     var chestplate: ItemStack? = chestplate
     var leggings: ItemStack? = leggings
@@ -267,6 +276,7 @@ class BukkitCharacter private constructor(id: Int, player: ElysiumPlayer, name: 
         if (location != character.location) return false
         // Probably incorrect - comparing Object[] arrays with Arrays.equals
         if (!Arrays.equals(inventoryContents, character.inventoryContents)) return false
+        if (if (offhand != null) offhand != character.offhand else character.offhand != null) return false
         if (if (helmet != null) helmet != character.helmet else character.helmet != null) return false
         if (if (chestplate != null) chestplate != character.chestplate else character.chestplate != null) return false
         if (if (leggings != null) leggings != character.leggings else character.leggings != null) return false
@@ -287,6 +297,7 @@ class BukkitCharacter private constructor(id: Int, player: ElysiumPlayer, name: 
         result = 31 * result + if (isDead) 1 else 0
         result = 31 * result + location.hashCode()
         result = 31 * result + Arrays.hashCode(inventoryContents)
+        result = 31 * result + if (offhand != null) offhand!!.hashCode() else 0
         result = 31 * result + if (helmet != null) helmet!!.hashCode() else 0
         result = 31 * result + if (chestplate != null) chestplate!!.hashCode() else 0
         result = 31 * result + if (leggings != null) leggings!!.hashCode() else 0
