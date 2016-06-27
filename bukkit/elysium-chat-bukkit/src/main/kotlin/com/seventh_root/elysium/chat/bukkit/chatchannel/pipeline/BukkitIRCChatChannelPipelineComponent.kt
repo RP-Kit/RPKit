58b8ce3948809ee6154739855b848d81fb3f7e22
Ross Binden <alyphen@gmail.com>
2016-06-27 14:45:43 +0100

    Fix compile errors

--- bukkit/elysium-chat-bukkit/src/main/kotlin/com/seventh_root/elysium/chat/bukkit/chatchannel/pipeline/BukkitIRCChatChannelPipelineComponent.kt
+++ bukkit/elysium-chat-bukkit/src/main/kotlin/com/seventh_root/elysium/chat/bukkit/chatchannel/pipeline/BukkitIRCChatChannelPipelineComponent.kt
@@ -3,6 +3,7 @@ package com.seventh_root.elysium.chat.bukkit.chatchannel.pipeline
 import com.seventh_root.elysium.api.chat.ChatChannelPipelineComponent
 import com.seventh_root.elysium.api.chat.ChatChannelPipelineComponent.Type.POST_PROCESSOR
 import com.seventh_root.elysium.api.chat.ChatMessageContext
+import com.seventh_root.elysium.api.chat.ChatMessagePostProcessContext
 
 class BukkitIRCChatChannelPipelineComponent(var ircChannel: String?, var isWhitelist: Boolean) : ChatChannelPipelineComponent() {
 
@@ -10,6 +11,10 @@ class BukkitIRCChatChannelPipelineComponent(var ircChannel: String?, var isWhite
         get() = POST_PROCESSOR
 
     override fun process(message: String, context: ChatMessageContext): String {
+        return message
+    }
+
+    override fun postProcess(message: String, context: ChatMessagePostProcessContext): String? {
         return message
     }
 
--- bukkit/elysium-chat-bukkit/src/main/kotlin/com/seventh_root/elysium/chat/bukkit/chatchannel/pipeline/BukkitLogChatChannelPipelineComponent.kt
+++ bukkit/elysium-chat-bukkit/src/main/kotlin/com/seventh_root/elysium/chat/bukkit/chatchannel/pipeline/BukkitLogChatChannelPipelineComponent.kt
@@ -3,20 +3,24 @@ package com.seventh_root.elysium.chat.bukkit.chatchannel.pipeline
 import com.seventh_root.elysium.api.chat.ChatChannelPipelineComponent
 import com.seventh_root.elysium.api.chat.ChatChannelPipelineComponent.Type.POST_PROCESSOR
 import com.seventh_root.elysium.api.chat.ChatMessageContext
+import com.seventh_root.elysium.api.chat.ChatMessagePostProcessContext
 import java.io.IOException
 
-class BukkitLogChatChannelPipelineComponent : ChatChannelPipelineComponent() {
+class BukkitLogChatChannelPipelineComponent: ChatChannelPipelineComponent() {
 
     override val type: ChatChannelPipelineComponent.Type
         get() = POST_PROCESSOR
 
     override fun process(message: String, context: ChatMessageContext): String {
+        return message
+    }
+
+    override fun postProcess(message: String, context: ChatMessagePostProcessContext): String {
         try {
             context.chatChannel.log(message)
         } catch (exception: IOException) {
             exception.printStackTrace()
         }
-
         return message
     }
 
