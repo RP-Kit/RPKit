85bbf787c7815b0da989a5f48386a8db85f8fe95
Ross Binden <ross.binden@gmail.com>
2017-01-06 23:12:02 +0000

    Fix caching bug with multiple currencies, fixes #138

--- bukkit/rpk-economy-bukkit/src/main/kotlin/com/rpkit/economy/bukkit/database/table/RPKWalletTable.kt
+++ bukkit/rpk-economy-bukkit/src/main/kotlin/com/rpkit/economy/bukkit/database/table/RPKWalletTable.kt
@@ -30,29 +30,22 @@ import org.ehcache.CacheManager
 import org.ehcache.config.builders.CacheConfigurationBuilder
 import org.ehcache.config.builders.CacheManagerBuilder
 import org.ehcache.config.builders.ResourcePoolsBuilder
+import java.sql.PreparedStatement
 import java.sql.Statement.RETURN_GENERATED_KEYS
 import java.util.*
 
 /**
  * Represents the wallet table.
  */
-class RPKWalletTable: Table<RPKWallet> {
+class RPKWalletTable(database: Database, private val plugin: RPKEconomyBukkit) : Table<RPKWallet>(database, RPKWallet::class.java) {
 
-    private val plugin: RPKEconomyBukkit
-    private val cacheManager: CacheManager
-    private val cache: Cache<Int, RPKWallet>
-    private val characterCache: Cache<Int, MutableMap<*, *>>
-
-    constructor(database: Database, plugin: RPKEconomyBukkit): super(database, RPKWallet::class.java) {
-        this.plugin = plugin;
-        cacheManager = CacheManagerBuilder.newCacheManagerBuilder().build(true)
-        cache = cacheManager.createCache("cache",
-                CacheConfigurationBuilder.newCacheConfigurationBuilder(Int::class.javaObjectType, RPKWallet::class.java,
-                        ResourcePoolsBuilder.heap(plugin.server.maxPlayers.toLong())).build())
-        characterCache = cacheManager.createCache("characterCache",
-                CacheConfigurationBuilder.newCacheConfigurationBuilder(Int::class.javaObjectType, MutableMap::class.java,
-                        ResourcePoolsBuilder.heap(plugin.server.maxPlayers.toLong())).build())
-    }
+    private val cacheManager: CacheManager = CacheManagerBuilder.newCacheManagerBuilder().build(true)
+    private val cache: Cache<Int, RPKWallet> = cacheManager.createCache("cache",
+            CacheConfigurationBuilder.newCacheConfigurationBuilder(Int::class.javaObjectType, RPKWallet::class.java,
+                    ResourcePoolsBuilder.heap(plugin.server.maxPlayers.toLong())).build())
+    private val characterCache: Cache<Int, MutableMap<*, *>> = cacheManager.createCache("characterCache",
+            CacheConfigurationBuilder.newCacheConfigurationBuilder(Int::class.javaObjectType, MutableMap::class.java,
+                    ResourcePoolsBuilder.heap(plugin.server.maxPlayers.toLong())).build())
 
     override fun create() {
         database.createConnection().use { connection ->
@@ -63,9 +56,7 @@ class RPKWalletTable: Table<RPKWallet> {
                             "currency_id INTEGER," +
                             "balance INTEGER" +
                     ")"
-            ).use { statement ->
-                statement.executeUpdate()
-            }
+            ).use(PreparedStatement::executeUpdate)
         }
     }
 
@@ -146,52 +137,54 @@ class RPKWalletTable: Table<RPKWallet> {
 
     fun get(character: RPKCharacter, currency: RPKCurrency): RPKWallet {
         if (characterCache.containsKey(character.id)) {
-            return get(characterCache[character.id][currency.id] as Int)!!
-        } else {
-            var wallet: RPKWallet? = null
-            database.createConnection().use { connection ->
-                connection.prepareStatement(
-                        "SELECT id, character_id, currency_id, balance FROM rpkit_wallet WHERE character_id = ? AND currency_id = ?"
-                ).use { statement ->
-                    statement.setInt(1, character.id)
-                    statement.setInt(2, currency.id)
-                    val resultSet = statement.executeQuery()
-                    if (resultSet.next()) {
-                        val id = resultSet.getInt("id")
-                        val characterId = resultSet.getInt("character_id")
-                        val currencyId = resultSet.getInt("currency_id")
-                        val balance = resultSet.getInt("balance")
-                        wallet = RPKWallet(
-                                id = id,
-                                character = plugin.core.serviceManager.getServiceProvider(RPKCharacterProvider::class).getCharacter(characterId)!!,
-                                currency = plugin.core.serviceManager.getServiceProvider(RPKCurrencyProvider::class).getCurrency(currencyId)!!,
-                                balance = balance
-                        )
-                        val finalWallet = wallet!!
-                        cache.put(id, finalWallet)
-                        val characterWallets: MutableMap<Int, Int>
-                        if (characterCache.containsKey(characterId)) {
-                            characterWallets = characterCache[characterId] as MutableMap<Int, Int>
-                        } else {
-                            characterWallets = HashMap<Int, Int>()
-                        }
-                        characterWallets.put(currencyId, finalWallet.id)
-                        characterCache.put(characterId, characterWallets)
+            val characterWallets = characterCache[character.id]
+            if (characterWallets.containsKey(currency.id)) {
+                return get(characterCache[character.id][currency.id] as Int)!!
+            }
+        }
+        var wallet: RPKWallet? = null
+        database.createConnection().use { connection ->
+            connection.prepareStatement(
+                    "SELECT id, character_id, currency_id, balance FROM rpkit_wallet WHERE character_id = ? AND currency_id = ?"
+            ).use { statement ->
+                statement.setInt(1, character.id)
+                statement.setInt(2, currency.id)
+                val resultSet = statement.executeQuery()
+                if (resultSet.next()) {
+                    val id = resultSet.getInt("id")
+                    val characterId = resultSet.getInt("character_id")
+                    val currencyId = resultSet.getInt("currency_id")
+                    val balance = resultSet.getInt("balance")
+                    wallet = RPKWallet(
+                            id = id,
+                            character = plugin.core.serviceManager.getServiceProvider(RPKCharacterProvider::class).getCharacter(characterId)!!,
+                            currency = plugin.core.serviceManager.getServiceProvider(RPKCurrencyProvider::class).getCurrency(currencyId)!!,
+                            balance = balance
+                    )
+                    val finalWallet = wallet!!
+                    cache.put(id, finalWallet)
+                    val characterWallets: MutableMap<Int, Int>
+                    if (characterCache.containsKey(characterId)) {
+                        characterWallets = characterCache[characterId] as MutableMap<Int, Int>
+                    } else {
+                        characterWallets = HashMap<Int, Int>()
                     }
+                    characterWallets.put(currencyId, finalWallet.id)
+                    characterCache.put(characterId, characterWallets)
                 }
             }
-            if (wallet == null) {
-                val finalWallet = RPKWallet(
-                        character = character,
-                        currency = currency,
-                        balance = currency.defaultAmount
-                )
-                insert(finalWallet)
-                wallet = finalWallet
-            }
-            val finalWallet = wallet!!
-            return finalWallet
         }
+        if (wallet == null) {
+            val finalWallet = RPKWallet(
+                    character = character,
+                    currency = currency,
+                    balance = currency.defaultAmount
+            )
+            insert(finalWallet)
+            wallet = finalWallet
+        }
+        val finalWallet = wallet!!
+        return finalWallet
     }
 
     fun getTop(amount: Int = 5, currency: RPKCurrency): List<RPKCharacter> {
@@ -211,7 +204,7 @@ class RPKWalletTable: Table<RPKWallet> {
                 }
             }
         }
-        return top.map { wallet -> wallet.character }
+        return top.map(RPKWallet::character)
     }
 
     override fun delete(entity: RPKWallet) {
