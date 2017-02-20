6cb225a290d6924371779c3384d627e4351224ee
Ross Binden <ross.binden@gmail.com>
2017-02-20 10:26:26 +0000

    #123 Update messages in rolling

--- bukkit/rpk-rolling-bukkit/src/main/kotlin/com/rpkit/rolling/bukkit/RPKRollingBukkit.kt
+++ bukkit/rpk-rolling-bukkit/src/main/kotlin/com/rpkit/rolling/bukkit/RPKRollingBukkit.kt
@@ -13,4 +13,12 @@ class RPKRollingBukkit: RPKBukkitPlugin() {
         getCommand("roll").executor = RollCommand(this)
     }
 
+    override fun setDefaultMessages() {
+        core.messages.setDefault("roll", "&f\$character/\$player rolled &7\$roll &ffrom &7\$dice")
+        core.messages.setDefault("roll-invalid-parse", "&cFailed to parse your roll.")
+        core.messages.setDefault("roll-usage", "&cUsage: /roll [roll]")
+        core.messages.setDefault("not-from-console", "&cYou must be a player to perform that command.")
+        core.messages.setDefault("no-character", "&cYou must have a character to perform that command.")
+    }
+
 }
