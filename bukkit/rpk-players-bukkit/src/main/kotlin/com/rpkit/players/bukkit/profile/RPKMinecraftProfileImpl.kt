441708e0355100f9f41c130319757191a4b366fc
Ross Binden <ross.binden@gmail.com>
2017-10-29 22:12:56 +0000

    Update from fanciful to Spigot text components, closes #134

--- bukkit/rpk-players-bukkit/src/main/kotlin/com/rpkit/players/bukkit/profile/RPKMinecraftProfileImpl.kt
+++ bukkit/rpk-players-bukkit/src/main/kotlin/com/rpkit/players/bukkit/profile/RPKMinecraftProfileImpl.kt
@@ -1,6 +1,22 @@
+/*
+ * Copyright 2017 Ross Binden
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.rpkit.players.bukkit.profile
 
-import mkremins.fanciful.FancyMessage
+import net.md_5.bungee.api.chat.BaseComponent
 import org.bukkit.Bukkit
 import java.util.*
 
@@ -20,11 +36,8 @@ class RPKMinecraftProfileImpl(
         Bukkit.getPlayer(minecraftUUID)?.sendMessage(message)
     }
 
-    override fun sendMessage(fancyMessage: FancyMessage) {
-        val bukkitPlayer = Bukkit.getPlayer(minecraftUUID)
-        if (bukkitPlayer != null) {
-            fancyMessage.send(bukkitPlayer)
-        }
+    override fun sendMessage(vararg components: BaseComponent) {
+        Bukkit.getPlayer(minecraftUUID)?.spigot()?.sendMessage(*components)
     }
 
 }
