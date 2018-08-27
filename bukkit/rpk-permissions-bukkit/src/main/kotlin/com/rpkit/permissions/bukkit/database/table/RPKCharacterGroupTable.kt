56d77926141b000d63019da362d6d7c6552a8370
Ross Binden <ross.binden@gmail.com>
2018-08-27 15:07:32 +0100

    #124 Allow caching to be customised for permissions

--- bukkit/rpk-permissions-bukkit/src/main/kotlin/com/rpkit/permissions/bukkit/database/table/RPKCharacterGroupTable.kt
+++ bukkit/rpk-permissions-bukkit/src/main/kotlin/com/rpkit/permissions/bukkit/database/table/RPKCharacterGroupTable.kt
@@ -18,9 +18,13 @@ import org.jooq.util.sqlite.SQLiteDataType
 
 class RPKCharacterGroupTable(database: Database, private val plugin: RPKPermissionsBukkit): Table<RPKCharacterGroup>(database, RPKCharacterGroup::class) {
 
-    private val cache = database.cacheManager.createCache("rpk-permissions-bukkit.rpkit_character_group.id",
-            CacheConfigurationBuilder.newCacheConfigurationBuilder(Int::class.javaObjectType, RPKCharacterGroup::class.java,
-                    ResourcePoolsBuilder.heap(20L)))
+    private val cache = if (plugin.config.getBoolean("caching.rpkit_character_group.id.enabled")) {
+        database.cacheManager.createCache("rpk-permissions-bukkit.rpkit_character_group.id",
+                CacheConfigurationBuilder.newCacheConfigurationBuilder(Int::class.javaObjectType, RPKCharacterGroup::class.java,
+                        ResourcePoolsBuilder.heap(plugin.config.getLong("caching.rpkit_character_group.id.size"))))
+    } else {
+        null
+    }
 
     override fun create() {
         database.create
@@ -54,7 +58,7 @@ class RPKCharacterGroupTable(database: Database, private val plugin: RPKPermissi
                 .execute()
         val id = database.create.lastID().toInt()
         entity.id = id
-        cache.put(id, entity)
+        cache?.put(id, entity)
         return id
     }
 
@@ -65,11 +69,11 @@ class RPKCharacterGroupTable(database: Database, private val plugin: RPKPermissi
                 .set(RPKIT_CHARACTER_GROUP.GROUP_NAME, entity.group.name)
                 .where(RPKIT_CHARACTER_GROUP.ID.eq(entity.id))
                 .execute()
-        cache.put(entity.id, entity)
+        cache?.put(entity.id, entity)
     }
 
     override fun get(id: Int): RPKCharacterGroup? {
-        if (cache.containsKey(id)) {
+        if (cache?.containsKey(id) == true) {
             return cache.get(id)
         } else {
             val result = database.create
@@ -92,7 +96,7 @@ class RPKCharacterGroupTable(database: Database, private val plugin: RPKPermissi
                         character,
                         group
                 )
-                cache.put(id, characterGroup)
+                cache?.put(id, characterGroup)
                 return characterGroup
             } else {
                 database.create
@@ -119,7 +123,7 @@ class RPKCharacterGroupTable(database: Database, private val plugin: RPKPermissi
                 .deleteFrom(RPKIT_CHARACTER_GROUP)
                 .where(RPKIT_CHARACTER_GROUP.ID.eq(entity.id))
                 .execute()
-        cache.remove(entity.id)
+        cache?.remove(entity.id)
     }
 
 }
