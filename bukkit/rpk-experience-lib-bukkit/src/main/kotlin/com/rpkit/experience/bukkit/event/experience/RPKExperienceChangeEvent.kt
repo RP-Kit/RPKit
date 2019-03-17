e5ac356eed687a95c2851a36085ef3c0dfaef356
Ross Binden <ross.binden@gmail.com>
2019-03-17 14:05:15 +0000

    #212 Add event-driven APIs to experience

---
+++ bukkit/rpk-experience-lib-bukkit/src/main/kotlin/com/rpkit/experience/bukkit/event/experience/RPKExperienceChangeEvent.kt
@@ -0,0 +1,29 @@
+/*
+ * Copyright 2019 Ross Binden
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.rpkit.experience.bukkit.event.experience
+
+import com.rpkit.characters.bukkit.character.RPKCharacter
+import com.rpkit.characters.bukkit.event.character.RPKCharacterEvent
+
+
+interface RPKExperienceChangeEvent: RPKCharacterEvent {
+
+    override val character: RPKCharacter
+    val oldExperience: Int
+    var experience: Int
+
+}
