7e6de90be3ca670b4da1bd9c8d56511bc38b1db6
Ren Binden <ren.binden@gmail.com>
2020-05-25 20:46:58 +0100

    Fix RPKBukkitChatChannelUpdateEvent missing async flag, closes #385

--- bukkit/rpk-chat-bukkit/src/main/kotlin/com/rpkit/chat/bukkit/listener/AsyncPlayerChatListener.kt
+++ bukkit/rpk-chat-bukkit/src/main/kotlin/com/rpkit/chat/bukkit/listener/AsyncPlayerChatListener.kt
@@ -56,7 +56,7 @@ class AsyncPlayerChatListener(private val plugin: RPKChatBukkit): Listener {
                                 }
                                 if (!chatChannel.listenerMinecraftProfiles.contains(minecraftProfile)) {
                                     chatChannel.addListener(minecraftProfile, event.isAsynchronous)
-                                    chatChannelProvider.updateChatChannel(chatChannel)
+                                    chatChannelProvider.updateChatChannel(chatChannel, event.isAsynchronous)
                                 }
                             }
                         }
