e242a16d0af7eb83288fe98b047193ac73165bdf
Ren Binden <ren.binden@gmail.com>
2020-05-28 15:49:39 +0100

    Remove item in hand null checks, closes #400

--- bukkit/rpk-essentials-bukkit/src/main/kotlin/com/rpkit/essentials/bukkit/command/EnchantCommand.kt
+++ bukkit/rpk-essentials-bukkit/src/main/kotlin/com/rpkit/essentials/bukkit/command/EnchantCommand.kt
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2020 Ren Binden
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.rpkit.essentials.bukkit.command
 
 import com.rpkit.essentials.bukkit.RPKEssentialsBukkit
@@ -14,7 +30,7 @@ class EnchantCommand(private val plugin: RPKEssentialsBukkit) : CommandExecutor 
     override fun onCommand(sender: CommandSender, cmd: Command, label: String, args: Array<String>): Boolean {
         if (sender.hasPermission("rpkit.essentials.command.enchant")) {
             if (sender is Player) {
-                if (sender.inventory.itemInMainHand != null && sender.inventory.itemInMainHand.type != Material.AIR) {
+                if (sender.inventory.itemInMainHand.type != Material.AIR) {
                     if (args.size >= 2) {
                         if (sender.hasPermission("rpkit.essentials.command.enchant.unsafe")) {
                             val enchantment = Enchantment.getByName(args[0].toUpperCase())
