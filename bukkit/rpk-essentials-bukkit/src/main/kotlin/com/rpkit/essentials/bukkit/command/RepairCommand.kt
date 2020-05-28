b08df87469912b737d3c2002d9dfec55fa8c95f8
Ren Binden <ren.binden@gmail.com>
2020-05-28 18:01:24 +0100

    Replace durability setting with ItemMeta, closes #398

--- bukkit/rpk-essentials-bukkit/src/main/kotlin/com/rpkit/essentials/bukkit/command/RepairCommand.kt
+++ bukkit/rpk-essentials-bukkit/src/main/kotlin/com/rpkit/essentials/bukkit/command/RepairCommand.kt
@@ -17,17 +17,24 @@
 package com.rpkit.essentials.bukkit.command
 
 import com.rpkit.essentials.bukkit.RPKEssentialsBukkit
+import org.bukkit.Bukkit
 import org.bukkit.command.Command
 import org.bukkit.command.CommandExecutor
 import org.bukkit.command.CommandSender
 import org.bukkit.entity.Player
+import org.bukkit.inventory.meta.Damageable
 
 class RepairCommand(private val plugin: RPKEssentialsBukkit) : CommandExecutor {
 
     override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<String>): Boolean {
         if (sender.hasPermission("rpkit.essentials.command.repair")) {
             if (sender is Player) {
-                sender.inventory.itemInMainHand.durability = 0.toShort()
+                val item = sender.inventory.itemInMainHand
+                val meta = item.itemMeta ?: Bukkit.getItemFactory().getItemMeta(item.type)
+                if (meta is Damageable) {
+                    meta.damage = 0
+                }
+                item.itemMeta = meta
                 sender.sendMessage(plugin.messages["repair-valid"])
             } else {
                 sender.sendMessage(plugin.messages["not-from-console"])
