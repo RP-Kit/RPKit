223962cbebda8b3fd24c95b0d967af87988bca3b
Ren Binden <ren.binden@gmail.com>
2022-04-16 08:07:30 +0100

    Add custom model data to itemmeta command, closes #555

--- bukkit/rpk-essentials-bukkit/src/main/kotlin/com/rpkit/essentials/bukkit/command/ItemMetaCommand.kt
+++ bukkit/rpk-essentials-bukkit/src/main/kotlin/com/rpkit/essentials/bukkit/command/ItemMetaCommand.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020 Ren Binden
+ * Copyright 2022 Ren Binden
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -27,62 +27,78 @@ import org.bukkit.entity.Player
 class ItemMetaCommand(private val plugin: RPKEssentialsBukkit) : CommandExecutor {
 
     override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<String>): Boolean {
-        if (sender.hasPermission("rpkit.essentials.command.itemmeta")) {
-            if (sender is Player) {
-                val itemInMainHand = sender.inventory.itemInMainHand
-                if (itemInMainHand.type != Material.AIR) {
-                    if (args.size >= 2) {
-                        val meta = itemInMainHand.itemMeta ?: plugin.server.itemFactory.getItemMeta(itemInMainHand.type)
-                        if (meta == null) {
-                            sender.sendMessage(plugin.messages["item=meta-failed-to-create"])
-                            return true
-                        }
-                        if (args[0].equals("setname", ignoreCase = true)) {
-                            val name = ChatColor.translateAlternateColorCodes('&', args.drop(1).joinToString(" "))
-                            meta.setDisplayName(name)
-                            sender.sendMessage(plugin.messages["item-meta-set-name-valid", mapOf(
-                                "name" to name
-                            )])
-                        } else if (args[0].equals("addlore", ignoreCase = true)) {
-                            val lore = meta.lore ?: mutableListOf<String>()
-                            val loreItem = ChatColor.translateAlternateColorCodes('&', args.drop(1).joinToString(" "))
-                            lore.add(loreItem)
-                            sender.sendMessage(plugin.messages["item-meta-add-lore-valid", mapOf(
-                                "lore" to loreItem
-                            )])
-                            meta.lore = lore
-                        } else if (args[0].equals("removelore", ignoreCase = true)) {
-                            if (meta.hasLore()) {
-                                val lore = meta.lore ?: mutableListOf<String>()
-                                val loreItem = ChatColor.translateAlternateColorCodes('&', args.drop(1).joinToString(" "))
-                                if (lore.contains(loreItem)) {
-                                    lore.remove(loreItem)
-                                    sender.sendMessage(plugin.messages["item-meta-remove-lore-valid", mapOf(
-                                        "lore" to loreItem
-                                    )])
-                                } else {
-                                    sender.sendMessage(plugin.messages["item-meta-remove-lore-invalid-lore-item"])
-                                }
-                                meta.lore = lore
-                            } else {
-                                sender.sendMessage(plugin.messages["item-meta-remove-lore-invalid-lore"])
-                            }
-                        } else {
-                            sender.sendMessage(plugin.messages["item-meta-usage"])
-                        }
-                        itemInMainHand.itemMeta = meta
-                    } else {
-                        sender.sendMessage(plugin.messages["item-meta-usage"])
-                    }
-                } else {
-                    sender.sendMessage(plugin.messages["item-meta-invalid-item"])
-                }
-            } else {
-                sender.sendMessage(plugin.messages["not-from-console"])
-            }
-        } else {
-            sender.sendMessage(plugin.messages["no-permission-item-meta"])
+        if (!sender.hasPermission("rpkit.essentials.command.itemmeta")) {
+            sender.sendMessage(plugin.messages.noPermissionItemMeta)
+            return true
         }
+        if (sender !is Player) {
+            sender.sendMessage(plugin.messages.notFromConsole)
+            return true
+        }
+        val itemInMainHand = sender.inventory.itemInMainHand
+        if (itemInMainHand.type == Material.AIR) {
+            sender.sendMessage(plugin.messages.itemMetaInvalidItem)
+            return true
+        }
+        if (args.size < 2) {
+            sender.sendMessage(plugin.messages.itemMetaUsage)
+            return true
+        }
+        val meta = itemInMainHand.itemMeta ?: plugin.server.itemFactory.getItemMeta(itemInMainHand.type)
+        if (meta == null) {
+            sender.sendMessage(plugin.messages.itemMetaFailedToCreate)
+            return true
+        }
+        when (args[0].lowercase()) {
+            "setname" -> {
+                val name = ChatColor.translateAlternateColorCodes('&', args.drop(1).joinToString(" "))
+                meta.setDisplayName(name)
+                sender.sendMessage(plugin.messages.itemMetaSetNameValid.withParameters(
+                    name = name
+                ))
+            }
+            "addlore" -> {
+                val lore = meta.lore ?: mutableListOf<String>()
+                val loreItem = ChatColor.translateAlternateColorCodes('&', args.drop(1).joinToString(" "))
+                lore.add(loreItem)
+                sender.sendMessage(plugin.messages.itemMetaAddLoreValid.withParameters(
+                    lore = loreItem
+                ))
+                meta.lore = lore
+            }
+            "removelore" -> {
+                if (!meta.hasLore()) {
+                    sender.sendMessage(plugin.messages.itemMetaRemoveLoreInvalidLore)
+                    return true
+                }
+                val lore = meta.lore ?: mutableListOf<String>()
+                val loreItem = ChatColor.translateAlternateColorCodes('&', args.drop(1).joinToString(" "))
+                if (!lore.contains(loreItem)) {
+                    sender.sendMessage(plugin.messages.itemMetaRemoveLoreInvalidLoreItem)
+                    return true
+                }
+                lore.remove(loreItem)
+                sender.sendMessage(plugin.messages.itemMetaRemoveLoreValid.withParameters(
+                    lore = loreItem
+                ))
+                meta.lore = lore
+            }
+            "custommodeldata" -> {
+                val customModelData = args[1].toIntOrNull()
+                if (customModelData == null) {
+                    sender.sendMessage(plugin.messages.itemMetaCustomModelDataInvalidCustomModelData)
+                    return true
+                }
+                meta.setCustomModelData(customModelData)
+                sender.sendMessage(plugin.messages.itemMetaCustomModelDataValid.withParameters(
+                    customModelData = customModelData
+                ))
+            }
+            else -> {
+                sender.sendMessage(plugin.messages.itemMetaUsage)
+            }
+        }
+        itemInMainHand.itemMeta = meta
         return true
     }
 
