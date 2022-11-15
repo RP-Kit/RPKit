cbb23baf7ef94b1ccb4ece0a043f0fcfd430b71c
Ren Binden <ren.binden@gmail.com>
2022-11-15 22:25:15 +0000

    Use reflection to add chat commands to command map, closes #623

--- bukkit/rpk-chat-bukkit/src/main/kotlin/com/rpkit/chat/bukkit/RPKChatBukkit.kt
+++ bukkit/rpk-chat-bukkit/src/main/kotlin/com/rpkit/chat/bukkit/RPKChatBukkit.kt
@@ -31,6 +31,7 @@ import com.rpkit.chat.bukkit.chatchannel.undirected.*
 import com.rpkit.chat.bukkit.chatgroup.RPKChatGroupService
 import com.rpkit.chat.bukkit.chatgroup.RPKChatGroupServiceImpl
 import com.rpkit.chat.bukkit.command.chatchannel.ChatChannelCommand
+import com.rpkit.chat.bukkit.command.chatchannel.QuickChatChannelCommand
 import com.rpkit.chat.bukkit.command.chatgroup.ChatGroupCommand
 import com.rpkit.chat.bukkit.command.listchatchannels.ListChatChannelsCommand
 import com.rpkit.chat.bukkit.command.message.MessageCommand
@@ -53,6 +54,7 @@ import com.rpkit.chat.bukkit.prefix.RPKPrefixServiceImpl
 import com.rpkit.chat.bukkit.snooper.RPKSnooperService
 import com.rpkit.chat.bukkit.snooper.RPKSnooperServiceImpl
 import com.rpkit.chat.bukkit.speaker.RPKChatChannelSpeakerService
+import com.rpkit.core.bukkit.command.toBukkit
 import com.rpkit.core.bukkit.listener.registerListeners
 import com.rpkit.core.database.Database
 import com.rpkit.core.database.DatabaseConnectionProperties
@@ -61,12 +63,17 @@ import com.rpkit.core.database.UnsupportedDatabaseDialectException
 import com.rpkit.core.plugin.RPKPlugin
 import com.rpkit.core.service.Services
 import org.bstats.bukkit.Metrics
+import org.bukkit.command.CommandMap
+import org.bukkit.command.PluginCommand
 import org.bukkit.configuration.file.YamlConfiguration
 import org.bukkit.configuration.serialization.ConfigurationSerialization
 import org.bukkit.permissions.Permission
 import org.bukkit.permissions.PermissionDefault
+import org.bukkit.plugin.Plugin
+import org.bukkit.plugin.SimplePluginManager
 import org.bukkit.plugin.java.JavaPlugin
 import java.io.File
+import java.lang.reflect.Constructor
 
 /**
  * RPK chat plugin default implementation.
@@ -225,6 +232,9 @@ class RPKChatBukkit : JavaPlugin(), RPKPlugin {
         getCommand("message")?.setExecutor(MessageCommand(this))
         getCommand("reply")?.setExecutor(ReplyCommand(this))
         getCommand("snoop")?.setExecutor(SnoopCommand(this))
+        Services[RPKChatChannelService::class.java]?.chatChannels?.forEach { chatChannel ->
+            getDynamicCommand(chatChannel.name.value).setExecutor(QuickChatChannelCommand(this, chatChannel).toBukkit())
+        }
     }
 
     private fun registerListeners() {
@@ -268,6 +278,21 @@ class RPKChatBukkit : JavaPlugin(), RPKPlugin {
                     PermissionDefault.FALSE
             ))
         }
+    }
+
+    private fun getCommandMap(): CommandMap {
+        val commandMapField = SimplePluginManager::class.java.getDeclaredField("commandMap")
+        commandMapField.isAccessible = true
+        return commandMapField.get(server.pluginManager) as CommandMap
+    }
+
+    private fun getDynamicCommand(name: String, vararg aliases: String): PluginCommand {
+        val constructor: Constructor<PluginCommand> = PluginCommand::class.java.getDeclaredConstructor(String::class.java, Plugin::class.java)
+        constructor.isAccessible = true
+        val command = constructor.newInstance(name, this)
+        command.aliases = aliases.toList()
+        getCommandMap().register(description.name, command)
+        return command
     }
 
 }
